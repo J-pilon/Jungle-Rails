d7baac7d0d9ba3b15dffb30bb0a163c3c50342ac
Josiah <josiah.pilon@yahoo.ca>
2021-05-07 04:13:01 +0000

    Tests for users name, email, password
    method .authenticate_with_credentials checks if email/password matches database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
-  # attr_accessible :email, :password, :password_confirmation
-  
-  validates_uniqueness_of :email
+    
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :name, :email, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    stripped_email = email.strip
+    
+    user = User.find_by(email: stripped_email) 
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
